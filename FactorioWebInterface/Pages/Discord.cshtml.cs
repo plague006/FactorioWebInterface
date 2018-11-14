7f7c87d7dde8fc372d045a126447a827ecbcaec4
grilledham <jrdgillham@gmail.com>
2018-11-14 16:04:12 +0000

    discord link redirect

---
+++ FactorioWebInterface/Pages/Discord.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FactorioWebInterface.Pages
+{
+    public class DiscordModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Redirect("https://discordapp.com/invite/HuzMbde");
+        }
+    }
+}
