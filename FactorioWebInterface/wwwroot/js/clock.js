a2471ea0c686f7182d8176f2e6405bfc203ad1e4
grilledham <jrdgillham@gmail.com>
2018-11-22 16:29:18 +0000

    added clock.js to source control

---
+++ FactorioWebInterface/wwwroot/js/clock.js
@@ -0,0 +1,11 @@
+﻿function GetClock() {
+    var d = new Date();
+    document.getElementById('time').innerHTML = d.toUTCString().substring(17, 26);
+}
+
+$(document).ready(function () {
+    GetClock();
+    setInterval(GetClock, 100);
+})
+
+
