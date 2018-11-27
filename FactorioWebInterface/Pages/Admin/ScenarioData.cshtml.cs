f9f547813c46a3a330df424e9ac2a7f7d72f5d4a
grilledham <jrdgillham@gmail.com>
2018-11-27 22:04:56 +0000

    redirect on ScenarioData page if user is not logged in

--- FactorioWebInterface/Pages/Admin/ScenarioData.cshtml.cs
+++ FactorioWebInterface/Pages/Admin/ScenarioData.cshtml.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FactorioWebInterface.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace FactorioWebInterface.Pages.Admin
 {
     public class ScenarioDataModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly UserManager<ApplicationUser> _userManger;
 
+        public ScenarioDataModel(UserManager<ApplicationUser> userManger)
+        {
+            _userManger = userManger;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManger.GetUserAsync(User);
+
+            if (user == null || user.Suspended)
+            {
+                HttpContext.Session.SetString("returnUrl", "scenariodata/");
+                return RedirectToPage("signIn");
+            }
+
+            return Page();
         }
     }
 }
