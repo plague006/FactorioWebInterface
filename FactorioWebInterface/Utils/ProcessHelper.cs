db20abf32497028e3e9c8062d570939c4e9c1f1e
grilledham <jrdgillham@gmail.com>
2019-02-15 21:04:34 +0000

    ProcessHelper class

---
+++ FactorioWebInterface/Utils/ProcessHelper.cs
@@ -0,0 +1,76 @@
+﻿using Serilog;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace FactorioWebInterface.Utils
+{
+    public static class ProcessHelper
+    {
+        public static bool RunProcessToEnd(string fileName, string arguments)
+        {
+            Log.Logger.Information("RunProcessToEnd filename: {fileName} arguments: {arguments}", fileName, arguments);
+
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = fileName, Arguments = arguments,
+                    UseShellExecute = false, CreateNoWindow = true,
+                }
+            };
+
+            try
+            {
+                process.Start();
+                process.WaitForExit();
+                return process.ExitCode > -1;
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Error(e, "RunProcessToEnd");
+                return false;
+            }
+            finally
+            {
+                process.Dispose();
+            }
+        }
+
+        public static Task<bool> RunProcessToEndAsync(string fileName, string arguments)
+        {
+            Log.Logger.Information("RunProcessToEndAsync filename: {fileName} arguments: {arguments}", fileName, arguments);
+
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = fileName, Arguments = arguments,
+                    UseShellExecute = false, CreateNoWindow = true,
+                },
+                EnableRaisingEvents = true
+            };
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            process.Exited += (sender, args) =>
+            {
+                tcs.SetResult(process.ExitCode > -1);
+                process.Dispose();
+            };
+
+            try
+            {
+                process.Start();
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Error(e, "RunProcessToEndAsync");
+                tcs.SetResult(false);
+                process.Dispose();
+            }
+
+            return tcs.Task;
+        }
+    }
+}
