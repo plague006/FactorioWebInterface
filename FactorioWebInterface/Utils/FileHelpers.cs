80881e7d0a3a3e3c4637badf788c8704a0f92759
grilledham <jrdgillham@gmail.com>
2019-02-16 11:20:27 +0000

    FileHelpers

---
+++ FactorioWebInterface/Utils/FileHelpers.cs
@@ -0,0 +1,24 @@
+﻿namespace FactorioWebInterface.Utils
+{
+    public static class FileHelpers
+    {
+        public static bool CreateDirectorySymlink(string targetPath, string linkPath)
+        {
+#if WINDOWS
+            return ProcessHelper.RunProcessToEnd("cmd.exe", $"/C MKLINK /D \"{linkPath}\" \"{targetPath}\"");
+#else
+            return ProcessHelper.RunProcessToEnd("/bin/ln", $"-s {targetPath} {linkPath}");      
+#endif
+        }
+
+        public static bool IsSymbolicLink(string filePath)
+        {
+#if WINDOWS
+            // Todo implement IsSymbolicLink for windows.
+            return false; // Hack: Assume it's not a symbolic link so that it will be deleted and recreated as one.
+#else
+            return ProcessHelper.RunProcessToEnd("/bin/bash", $"-c \"if [ ! -L {filePath} ] ; then exit 1 ; fi\"");
+#endif
+        }
+    }
+}
