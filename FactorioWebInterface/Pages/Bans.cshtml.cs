6d58ecfb69c0eabec2b4059f4666cb916c60dd68
grilledham <jrdgillham@gmail.com>
2019-02-23 11:54:28 +0000

    Add public ban page

---
+++ FactorioWebInterface/Pages/Bans.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using FactorioWebInterface.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FactorioWebInterface.Pages
+{
+    public class BansModel : PageModel
+    {
+        private readonly IFactorioServerManager _factorioServerManager;
+
+        public BansModel(IFactorioServerManager factorioServerManager)
+        {
+            _factorioServerManager = factorioServerManager;
+        }
+
+        public List<string> Bans { get; private set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Bans = await _factorioServerManager.GetBanUserNamesAsync();
+
+            return Page();
+        }
+    }
+}
